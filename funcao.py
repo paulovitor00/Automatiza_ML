d12e72c75b3bb0dc7b75b65948ad2e0b88d8c215
paulovitor00 <paulovitor.00@outlook.com>
2022-10-06 23:25:07 -0300

    Criação da função que acessa o email e realiza o envio do print contendo um resumo das vendas

--- funcao.py
+++ funcao.py
@@ -1,33 +1,82 @@
+import datetime
+
 import pyautogui                                                        # Import pyautogui.
+from datetime import date
 import time
+import pyscreenshot as ImageGrab
 
 
 def main():                                                             # Main function.
-    openSafari()                                                        # Call openChrome.
-    openWebsites('www.mercadolivre.com.br')                             # Call open websites with the websites.
+    opensafari()                                                        # Call openChrome.
+    openwebsites('www.mercadolivre.com.br')                             # Call open websites with the websites.
     time.sleep(10)
-    acessarVendas()
+    acessarvendas()
+    printsituacao()
+    enviosituacao()
 
 
-def openSafari():                                                       # Function to open chrome.
-    pyautogui.click(x=153, y=862)                                       #clica no icone do safari
+def opensafari():                                                       # Função para abrir o safari no macOS.
+    pyautogui.click(x=153, y=862)                                       # clica no icone do safari
     time.sleep(3)
-    pyautogui.click(x=129, y=51)                                        #aumenta a janela
+    pyautogui.click(x=129, y=51)                                        # aumenta a janela
     time.sleep(3)
-    pyautogui.click(x=1374, y=25)                                       #abre uma nova aba
+    pyautogui.click(x=1374, y=25)                                       # abre uma nova aba
 
 
-def openWebsites(url):                                                 # Funcao que abre o website
-        pyautogui.typewrite(url, interval=0.001)                       # Write the website with a 0.001 delay.
-        pyautogui.typewrite(['enter'])                                 # Press enter.
+def openwebsites(url):                                                 # Funcao que abre o website do mercado Livre
+    pyautogui.typewrite(url, interval=0.001)                           # Write the website with a 0.001 delay.
+    pyautogui.typewrite(['enter'])                                     # Press enter.
 
 
-def acessarVendas():                                                    #função para acessar a aba 'vendas'
-    pyautogui.moveTo(x=1036, y=159)                                     #clica no nome de usuario
+def acessarvendas():                                                    # função para acessar a aba 'vendas'
+    pyautogui.moveTo(x=1036, y=159)                                     # clica no nome de usuario
     time.sleep(1)
-    pyautogui.click(x=920, y=677)                                       #clica na opção 'vendas
+    pyautogui.click(x=920, y=677)                                       # clica na opção 'vendas
 
-def printSituacao():
-    Image = pyautogui.screenshot('SituacaoVendas.png', region=(424, 427, 418, 579))
+
+def printsituacao():
+    time.sleep(4)
+    im = ImageGrab.grab()
+    im.save("situaçãovendas.png")
+    time.sleep(2)
+
+
+def enviosituacao():
+    time.sleep(2)
+    pyautogui.click(x=1374, y=25)
+    emailurl = 'www.outlook.com'
+    pyautogui.typewrite(emailurl, interval=0.001)
+    pyautogui.typewrite(['enter'])
+    time.sleep(5)
+    pyautogui.click(x=209, y=146)
+    time.sleep(5)
+    email = 'jeancarlossilva2015@hotmail.com'
+    pyautogui.typewrite(email, interval=0.002)
+    pyautogui.typewrite(['enter'])
+    time.sleep(1)
+    pyautogui.typewrite(['tab'])
+    time.sleep(1)
+    pyautogui.typewrite(f' RESUMO DAS VENDAS PLAT: MERCADO LIVRE - DATA:  {datetime.date.today()}',  interval=0.002)
+    time.sleep(1)
+    pyautogui.click(x=888, y=640)
+    time.sleep(1)
+    pyautogui.click(x=968, y=536)
+    time.sleep(2)
+    pyautogui.click(x=942, y=235)
+    pyautogui.typewrite('SituacaoVendas', interval=0.001)
+    time.sleep(1)
+    pyautogui.click(x=595, y=285)
+    time.sleep(1)
+    pyautogui.click(x=797, y=375)
+    # pyautogui.click(x=1016, y=685)
+
+
+
+
+
+
+
+
+
 
 
--- main.py
+++ main.py
@@ -6,3 +6,4 @@ funcao.main()
 
 
 
+
